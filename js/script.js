7ce1ac594ea9d48930e311cf81520a23319670a4
NikoShivkumar <159164670+NikoShivkumar@users.noreply.github.com>
2024-04-29 15:05:31 +0000

    u

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Niko
+// Created on: april 2024
 // This file contains the JS functions for index.html
+
+
+"use strict"
+//function to see what rating of movies user can watch
+function discountService() {
+
+  // get user input and declare it as a variable for their age
+  let userAge = document.getElementById("age").value;
+  
+
+  // Check if user input is empty
+  if (userAge) || (select == 0) {
+    document.getElementById("user-info").innerHTML = "Please fill out both questions";
+    return;
+
+  let select = document.getElementById("day").value;
+  let day = select.options[select.selectedIndex].value;
+
+  if (day == "Tuesday") || (day == "Thursday") {
+    document.getElementById("user-info").innerHTML = "You are elegible for a discount"
+  }
+  else if (userAge >= 12) && (userAge <= 21) {
+    document.getElementById("user-info").innerHTML = "You are elegible for a discount"
+  }
+  else {
+    document.getElementById("userinfo ")
+  }
+
+}
